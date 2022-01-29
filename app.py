57bfb9d7b36ddc8e28c524fcc407daecfa669342
nknsryo <nkns.ryo@gmail.com>
2022-01-29 16:18:24 +0900

    関数を別のフォルダで管理する。

--- app.py
+++ app.py
@@ -1,20 +1,9 @@
-import requests
-
-
-def search_address(zipcode):
-    url = f"https://zipcloud.ibsnet.co.jp/api/search?zipcode={zipcode}"
-    response = requests.get(url)
-    results = response.json()['results']
-    pref_name = results[0]['address1']
-    city_name = results[0]['address2']
-    town_name = results[0]['address3']
-    address = f"{pref_name}{city_name}{town_name}"
-    return address
+from search_address import search_address
 
 
 def main():
-    # zipcode = input("郵便番号を入力してください(ハイフン無し7桁) >> :")
-    zipcode = "4443174"
+    zipcode = input("郵便番号を入力してください(ハイフン無し7桁) >> :")
+    # zipcode = "4443174"
     address = search_address(zipcode)
     print(address)
 
