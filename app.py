b07f7ed4e65edc207deb7e475718dd2b946d0dec
nknsryo <nkns.ryo@gmail.com>
2022-01-29 15:55:06 +0900

    関数化、リファクタリングした。

--- app.py
+++ app.py
@@ -1,21 +1,23 @@
 import requests
 
 
-def main():
-    # zipcode = input("郵便番号を入力してください(ハイフン無し7桁) >> :")
-    zipcode = "4443174"
+def search_address(zipcode):
     url = f"https://zipcloud.ibsnet.co.jp/api/search?zipcode={zipcode}"
     response = requests.get(url)
     results = response.json()['results']
-
     pref_name = results[0]['address1']
     city_name = results[0]['address2']
     town_name = results[0]['address3']
+    address = f"{pref_name}{city_name}{town_name}"
+    return address
 
-    return f"{pref_name}{city_name}{town_name}"
 
+def main():
+    # zipcode = input("郵便番号を入力してください(ハイフン無し7桁) >> :")
+    zipcode = "4443174"
+    address = search_address(zipcode)
+    print(address)
 
-# print(main())
 
 if __name__ == "__main__":
     main()
